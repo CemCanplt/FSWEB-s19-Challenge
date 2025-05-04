7f8b70afda96377a14edf6fda84a4b2b4c36a56a
CemCanplt <cem--canpolat@hotmail.com>
2025-05-04 21:09:43 +0300

    Başladık

---
+++ src/main/java/com/twitter/mavikus/MavikusApplication.java
@@ -0,0 +1,13 @@
+package com.twitter.mavikus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavikusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavikusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twitter/mavikus/MavikusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.mavikus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavikusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
