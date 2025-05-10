e7924222f7a8b128b36755ec9b11002a0d230045
CemCanplt <cem--canpolat@hotmail.com>
2025-05-10 21:14:34 +0300

    Unit Test'e başlıyorum

---
+++ src/test/java/com/twitter/mavikus/repository/LikeRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.twitter.mavikus.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LikeRepositoryTest {
+
+    @DisplayName("Can find tweet by user id?")
+    @Test
+    void findByTweetIdAndUserId() {
+    }
+
+    @DisplayName("Can find tweet by user id?")
+    @Test
+    void findByTweetId() {
+    }
+
+    @Test
+    void findByUserId() {
+    }
+}
